9da47a7c1622c7cbb0d60155395fcfde113bffb9
UnderT0p <70749855+UnderT0p@users.noreply.github.com>
2021-06-23 17:53:02 +0300

    Update install_up.sh

--- install_up.sh
+++ install_up.sh
@@ -17,6 +17,8 @@ sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/dock
 sudo yum install docker-ce docker-ce-cli containerd.io -y
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+sudo systemctl start docker
+
 elif grep -q Ubuntu  /etc/os-release
 then 
 sudo apt-get update
