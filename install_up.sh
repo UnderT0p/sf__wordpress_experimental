f309b3f527b2b06cd1909438606ffe8a8e4bf092
UnderT0p <70749855+UnderT0p@users.noreply.github.com>
2021-06-24 14:05:29 +0300

    Update install_up.sh

--- install_up.sh
+++ install_up.sh
@@ -10,7 +10,7 @@ sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-bash wp_up.sh
+bash ~/sf__wordpress_staging/wp_up.sh
 
 elif grep -q CentOS  /etc/os-release
 then 
@@ -19,8 +19,9 @@ sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/dock
 sudo yum install docker-ce docker-ce-cli containerd.io -y
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 sudo systemctl start docker
-bash wp_up.sh
+bash ~/sf__wordpress_staging/wp_up.sh
 
 elif grep -q Ubuntu  /etc/os-release
 then 
@@ -33,7 +34,6 @@ sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-bash wp_up.sh
+bash ~/sf__wordpress_staging/wp_up.sh
 else echo "Sorry we don't know your os"
 fi
-
